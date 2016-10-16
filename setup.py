2b018d45ef2f8ab588acb76fa6eb08def23978d6
Naftuli Tzvi Kay <rfkrocktk@gmail.com>
2016-10-16 00:33:27 -0700

    Add Buildout Project Scaffolding
    Here, we are adding the bare minimum requirements to get
    Buildout working for our project. Buildout is a build tool and
    dependency management system to make it easy to make highly
    customized (and self-contained!) Python projects.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "tastyscrapy",
+    version = "0.0.1",
+    packages = find_packages('src'),
+    package_dir = { '': 'src' },
+    install_requires = [
+        'setuptools',
+    ],
+)
